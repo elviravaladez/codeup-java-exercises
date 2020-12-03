6631be6f464f91fb1c84dcd00739a3fdc7492097
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-12-03 11:49:01 -0600

    adding ControlStatementsLec notes

--- src/ControlStatementsLec.java
+++ src/ControlStatementsLec.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlStatementsLec {
     public static void main(String[] args) {
         // ==================== Boolean Expressions
@@ -21,32 +23,74 @@ public class ControlStatementsLec {
         // same comparison process for || and |
 
         // x will only increment in second condition if first condition is true using &&
+
+        //ORIGINAL CODE:
 //                int x = 2;
 //                int y = 3;
-
-//                if (true && ++x == y) {
+//
+//                if (true && ++x == y) { //Prints Equal
 //                    System.out.println("Equal");
 //                } else {
 //                    System.out.println("Not equal");;
 //                }
-//                System.out.println(x);
+//                System.out.println(x); //Prints 3
+
+
+//        //ALTERED CODE:
+//        int x = 2;
+//        int y = 3;
+//
+//        if (false && ++x == y) { //Prints Not equal
+//            System.out.println("Equal");
+//        } else {
+//            System.out.println("Not equal");;
+//        }
+//        System.out.println(x); //Prints 2
+
+
+
+//        //ALTERED CODE:
+//        int x = 2;
+//        int y = 3;
+//
+//        if (false & ++x == y) { //Prints Not equal
+//            System.out.println("Equal");
+//        } else {
+//            System.out.println("Not equal");;
+//        }
+//        System.out.println(x); //Prints 3
+
+
+
 
 
         // ==================== Slide 7
 
         // DO NOT COMPARE STRINGS WITH == !!!!
 
+
+        //This is b/c we use == to compare primitive types! Each String is a different object.
+
 //                Scanner sc = new Scanner(System.in);
-//                String cat1 = sc.nextLine();
-//                String cat2 = "cat";
+//                String cat1 = sc.nextLine(); //object
+//                String cat2 = "cat"; //object
 //                System.out.println(cat1);
 //                System.out.println(cat2);
 //                System.out.println(cat1 == cat2);
 
-        // do use equals(),
 
+
+        //USE THIS CODE INSTEAD: using the .equals()
+        // do use equals()
+
+
+//    //ORIGINAL CODE:
 //    boolean stringValuesAreEqual = "Test".equals("Tes");
-//    System.out.printf("The result is: %s", stringValuesAreEqual);
+//    System.out.printf("The result is: %s", stringValuesAreEqual); //false
+
+        //ALTERED CODE:
+//        boolean stringValuesAreEqual = "Test".equals("Test");
+//        System.out.printf("The result is: %s", stringValuesAreEqual); //true
 
 
         // or use equalsIgnoreCase()
@@ -66,17 +110,19 @@ public class ControlStatementsLec {
         // basically the same as JS :)
 
 
-//            String caseSwitch = "Fred";
-//            switch (caseSwitch) {
-//                case "bob":
-//                    System.out.println("Case 1");
-//                    break;
-//                case "fred":
-//                    System.out.println("Case 2");
-//                    break;
-//                default :
-//                    System.out.println("Default case");
-//                    break;
-//            }
+            String caseSwitch = "Fred";
+            switch (caseSwitch) {
+                case "bob":
+                    System.out.println("Case 1");
+                    break;
+                case "fred":
+                    System.out.println("Case 2");
+                    break;
+                default :
+                    System.out.println("Default case");
+                    break;
+            }
+
+            //OUTPUT WOULD BE: Default case
     }
 }
