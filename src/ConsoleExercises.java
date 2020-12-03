57ea0b86a743abfdbb9563fedb9653dd23673051
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-12-02 18:16:53 -0600

    exercise 1: Using System.out.format to output aprroximate value of pi

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -2,12 +2,12 @@ public class ConsoleExercises {
     public static void main(String[] args) {
         //CONSOLE IO EXERCISES
 
-        //1. Copy this code into your main method: double pi = 3.14159;
+        //Exercise 1. Copy this code into your main method: double pi = 3.14159;
         double pi = 3.14159;
 
         //Write some Java code that uses the variable pi to output the following:
         //The value of pi is approximately 3.14.
-
+        System.out.format("The value of pi is approximately %.2f.%n", pi);
 
 
         //Don't change the value of the variable; instead, reference one of the
