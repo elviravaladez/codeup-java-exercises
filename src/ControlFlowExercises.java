1e2d3e1392ccc63d2a4285662e0338efed18d5f8
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-12-03 09:29:10 -0600

    Exercise 1b Do While TODO 1: do-while loop counts by 2s ending at 100

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,17 +4,43 @@ public class ControlFlowExercises {
 
         //TODO: EXERCISE 1: LOOP BASICS
         // a. While
-        //      Create an integer variable i with a value of 5.
-        //      Create a while loop that runs so long as i is less than or equal to 15
-        //      Each loop iteration, output the current value of i, then increment i by one.
+        //      -Create an integer variable i with a value of 5.
+        //      -Create a while loop that runs so long as i is less than or equal to 15
+        //      -Each loop iteration, output the current value of i, then increment i by one.
         // Your output should look like this:
         // 5 6 7 8 9 10 11 12 13 14 15
 
-        int i = 5;
+//        //CODE:
+//        int i = 5;
+//
+//        while(i <= 15) {
+//            System.out.println("i is " + i);
+//            i++;
+//        }
 
-        while(i <= 15) {
-            System.out.println("i is " + i);
+
+        //TODO: b. Do While.
+        //      -TODO: Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+
+        //CODE:
+        int i = 0;
+
+        do {
+            if(i % 2 == 0) {
+                System.out.println(i);
+            }
             i++;
-        }
+        } while(i <= 100);
+
+
+        //      -TODO: Alter your loop to count backwards by 5's from 100 to -10.
+        //      -TODO: Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
+        //          2
+        //          4
+        //          16
+        //          256
+        //          65536
+
+
     }
 }
