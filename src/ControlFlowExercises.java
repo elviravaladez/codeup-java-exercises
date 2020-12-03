753f723f8e22a27893759d929b4d7da31d40a14f
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-12-03 14:43:44 -0600

    exercise 3: displaying a table of powers

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
         //TODO: For all of the following exercises, create a new class named ControlFlowExercises with a main method. After each exercise, commit your changes, then replace your code in the main method with the next exercise.
@@ -84,18 +86,55 @@ public class ControlFlowExercises {
         //      -For the multiples of five: print “Buzz”.
         //      -For numbers which are multiples of both three and five: print “FizzBuzz”.
 
-        //CODE:
-        for(int i = 1; i <= 100; i++) {
-            if((i % 15) == 0) {
-                System.out.println("FizzBuzz");
-            } else if((i % 3) == 0) {
-                System.out.println("Fizz");
-            } else if((i % 5) == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
-            }
+//        //CODE:
+//        for(int i = 1; i <= 100; i++) {
+//            if((i % 15) == 0) {
+//                System.out.println("FizzBuzz");
+//            } else if((i % 3) == 0) {
+//                System.out.println("Fizz");
+//            } else if((i % 5) == 0) {
+//                System.out.println("Buzz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+
+
+        // TODO: EXERCISE 3: Display a table of powers.
+        //          -Prompt the user to enter an integer.
+        //          -Display a table of squares and cubes from 1 to the value entered.
+        //          -Ask if the user wants to continue.
+        //          -Assume that the user will enter valid data.
+        //          -Only continue if the user agrees to.
+        //     Example Output:
+        //What number would you like to go up to? 5
+        //Here is your table!
+        //
+        //number | squared | cubed
+        //------ | ------- | -----
+        //1      | 1       | 1
+        //2      | 4       | 8
+        //3      | 9       | 27
+        //4      | 16      | 64
+        //5      | 25      | 125
+
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("What number would you like to go up to? ");
+
+        int startValue = userInput.nextInt(); //This will capture the int up to the first white space
+        System.out.println("\nHere is your table!\n");
+        System.out.println("number | squared | cubed \n------ | ------- | -----");
+
+        for (int i = 1; i <= startValue; i++) {
+
+            int numberSquared = (int) Math.pow(i, 2);
+            int numberCubed = (int) Math.pow(i, 3);
+
+            System.out.printf("%-6d | %-7d | %-6d\n", i, numberSquared, numberCubed);
+
         }
+
 
     }
 }
