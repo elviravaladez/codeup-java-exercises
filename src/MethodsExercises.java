ba5338100f85d28faf9adb34f68e87f6076630b1
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-12-03 22:38:06 -0600

    initial commit for Methods Exercises

---
+++ src/MethodsExercises.java
@@ -0,0 +1,32 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+        // TODO: METHODS EXERCISES
+        //      Create a class named MethodsExercises.
+        //      Inside of your class, write code to create the specified methods.
+        //      Test your code by creating a main method
+        //      and calling each of the methods you've created.
+
+
+        // TODO: EXERCISE 1: BASIC ARITHMETIC
+
+        //   TODO: A) Create four separate methods. Each will perform an arithmetic operation:
+        //       -Addition
+        //       -Subtraction
+        //       -Multiplication
+        //       -Division
+
+
+
+        // TODO: B) Each function needs to take two parameters/arguments so that the operation can be performed.
+
+
+        // TODO: C) Test your methods and verify the output.
+
+
+        // TODO: D) Add a modulus method that finds the modulus of two numbers.
+        //Food for thought: What happens if we try to divide by zero? What should happen?
+
+
+
+    }
+}
