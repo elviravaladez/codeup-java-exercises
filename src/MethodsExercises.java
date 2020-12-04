02fb5b33664518bb680293d2d8cb4525896d05bf
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-12-03 23:50:37 -0600

    adding descriptions for method exercises 2-4

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -51,6 +51,54 @@ public class MethodsExercises {
         System.out.println(numOne % numTwo);
     }
 
+
+
+    // TODO: EXERCISE 2: Create a method that validates that user input is in a certain range
+    //  -The method signature should look like this:
+    //          public static int getInteger(int min, int max);
+    //  and is used like this:
+    //          System.out.print("Enter a number between 1 and 10: ");
+    //          int userInput = getInteger(1, 10);
+    //  If the input is invalid, prompt the user again. Hint: recursion might be helpful here!
+
+
+
+
+
+
+
+    //TODO: EXERCISE 3: Calculate the factorial of a number.
+    //      -Prompt the user to enter an integer from 1 to 10.
+    //      -Display the factorial of the number entered by the user.
+    //      -Ask if the user wants to continue.
+    //      -Use a for loop to calculate the factorial.
+    //      -Assume that the user will enter an integer, but verify it’s between 1 and 10.
+    //      -Use the long type to store the factorial.
+    //      -Continue only if the user agrees to.
+    //      -A factorial is a number multiplied by each of the numbers before it.
+    //      -Factorials are denoted by the exclamation point (n!). Ex:
+    //          1! = 1               = 1
+    //          2! = 1 x 2           = 2
+    //          3! = 1 x 2 x 3       = 6
+    //          4! = 1 x 2 x 3 x 4   = 24
+
+
+
+
+
+
+    //TODO: EXERCISE 4: Create an application that simulates dice rolling.
+    //      -Ask the user to enter the number of sides for a pair of dice.
+    //      -Prompt the user to roll the dice.
+    //      -"Roll" two n-sided dice, display the results of each, and then ask the user if he/she wants to roll the dice again.
+    //      -Use static methods to implement the method(s) that generate the random numbers.
+    //      -Use the .random method of the java.lang.Math class to generate random numbers.
+
+
+
+
+
+
     //MAIN METHOD
     public static void main(String[] args) {
         addition(10,10);
