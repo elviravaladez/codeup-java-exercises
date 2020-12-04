262819136a9087ee0bfae23b28c27d1db7635ad5
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-12-03 19:25:54 -0600

    Exercise 1: String Basics

---
+++ src/StringExercise.java
@@ -0,0 +1,47 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        // TODO: STRING EXERCISES
+
+        //TODO: Exercise 1: String Basics: Create a class named StringExercise with a main method.
+        //      -For each of the following output examples:
+        //              create a String variable
+        //              that contains the desired output and
+        //              print it out to the console,
+        //              you can do this with only one String variable and
+        //              one print statement for each output example.
+        //OUTPUT 1 EXAMPLE
+        //We don't need no education
+        //We don't need no thought control
+
+        //OUTPUT 2 EXAMPLE
+        //Check "this" out!, "s inside of "s!
+
+        //OUTPUT 3 EXAMPLE
+        //In windows, the main drive is usually C:\
+
+        //OUTPUT 4 EXAMPLE
+        // I can do backslashes \, double backslashes \\,
+        //and the amazing triple backslash \\\!
+
+
+        //CODE:
+
+        //OUTPUT 1:
+        String output1 = "We don't need no education\nWe don't need no thought control";
+        System.out.println(output1);
+
+        //OUTPUT 2:
+        String output2 = "Check " + "\"" + "this" + "\"" + " out!, " + "\"" + "s inside of " + "\"" + "s!";
+        System.out.println(output2);
+
+        //OUTPUT 3:
+        String output3 = "In windows, the main drive is usually C:" + "\\";
+        System.out.println(output3);
+
+        //OUTPUT 4:
+        String output4 = "I can do backslashes " + "\\" + ", double backslashes " + "\\" + "\\" + ",\n" + "and the amazing triple backslash " + "\\" + "\\" + "\\" + "!";
+        System.out.println(output4);
+
+
+    }
+}
