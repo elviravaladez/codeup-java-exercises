8099e0092a253d3436ec41a7c70c3626445aec7c
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-12-11 00:59:01 -0600

    refactoring code to remove comments

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -2,28 +2,16 @@ import java.util.Arrays;
 
 public class ArraysExercises {
 
-    //TODO: ARRAY BASIC
-    // Create a class inside of src named ArraysExercises.
-    // Create a main method for this class to do your work.
-
-    //Main Method
     public static void main(String[] args) {
-        // TODO: What happens when you run the following code? Why is Arrays.toString necessary?
 //        int[] numbers = {1, 2, 3, 4, 5};
-//
-//        System.out.println(numbers); //the memory location is the output
+//        System.out.println(numbers); //the array object is not the output
 
-        //TODO: Create an array that holds 3 Person objects.
-        // Assign a new instance of the Person class to each
-        // element.
         Person[] people = new Person[3];
 
         people[0] = new Person("Ethan");
         people[1] = new Person("Elvira");
         people[2] = new Person("Ryan");
 
-        // TODO: Iterate through the array and print out
-        //  the name of each person in the array.
         for (Person person : people) {
             System.out.println(person.getName());
         }
@@ -31,14 +19,6 @@ public class ArraysExercises {
         addPerson(people, new Person("Tom"));
     }
 
-    //TODO: Create a static method named addPerson.
-    // It should accept an array of Person objects,
-    // as well as a single person object to add to the
-    // passed array. It should return an array whose
-    // length is 1 greater than the passed array, with
-    // the passed person object at the end of the array.
-
-    //AddPerson Method
     public static Person[] addPerson(Person[] people, Person person) {
         int newArrLength = people.length + 1;
 
